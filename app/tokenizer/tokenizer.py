e82a9e46a9b9e0ec4c29b6cf9c1079100fab93cb
fufex3934 <fufawakgari174@gmail.com>
2024-02-12 20:51:53 +0300

    correcting final version of tokenizer pattern

--- app/tokenizer/tokenizer.py
+++ app/tokenizer/tokenizer.py
@@ -2,7 +2,7 @@ import re
 
 def tokenize(text):
     # Define a regular expression for tokenizing words (possibly followed by a punctuation mark), digits, and standalone punctuation marks
-    pattern = re.compile(r'\b[\w\'-]+(?:[.,;:!?])?\b|\d+|[.,;:!?()!]')
+    pattern = re.compile(r'\b[\w\'-]+(?:[.,;:!?\"\'])?\b|\d+|[.,;:!?()\"\']|\s')
 
     # Use the regular expression to find all tokens in the input text
     tokens = pattern.findall(text)
